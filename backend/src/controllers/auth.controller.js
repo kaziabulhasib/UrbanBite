fbc4f799ec85d00f48711cd3649b20f6f292a7c3
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-04 11:06:01 +0530

    file structure re-arrange

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userModel = require("../models/user.model");
+
+async function registerUser(req, res) {
+  const { fullName, email, password } = req.body;
+
+  const isUserAlreadyExists = await userModel.findOne({
+    email,
+  });
+
+  if (isUserAlreadyExists) {
+    return res.status(400).json({
+      message: "user already exists",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    fullName,
+    email,
+    password: hashedPassword,
+  });
+
+  const token =jwt.sign()
+}
