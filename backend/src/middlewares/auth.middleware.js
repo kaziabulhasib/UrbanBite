c7bc72e831c8e0e4873f9c96994f05132428fa2d
kaziabulhasib <hasibkazi420@gmail.com>
2025-09-08 18:05:19 +0530

    feat : auth user middleware created

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const foodpartnerModel = require("../models/foodpartner.model");
+const userModel = require("../models/user.model");
 
 async function AuthFoodPartnerMiddleware(req, res, next) {
   const token = req.cookies["food-token"];
@@ -13,6 +14,26 @@ async function AuthFoodPartnerMiddleware(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const foodPartner = await foodpartnerModel.findById(decoded.id);
     req.foodPartner = foodPartner;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "please login first",
+    });
+  }
+}
+
+async function AuthUserMiddleware(req, res, next) {
+  const token = req.cookies["food-token"];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "please login first",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.id);
+    req.user = user;
     next();
   } catch (error) {
     return res.status(401).json({
